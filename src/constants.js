e49c67a9acea25b464421e9e98547cd0e0a282d8
driyant <message.driyant@outlook.com>
2024-04-16 11:58:32 +0700

    feat: fix double request api call

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const generateBaseURL = (option) => {
   // return `https://center-gateway.helloriyan.my.id/api/${option}-search-engine`;
   return process.env.NODE_ENV === "development"
     ? `/api/${option}-search-engine.json`
-    : `https://center-gateway.helloriyan.my.id/api/${option}-search-engine`;
+    : `https://center-gateway.svrhr.my.id/api/${option}-search-engine`;
 };
